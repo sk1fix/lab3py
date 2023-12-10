d0e07aa04c549fcc2ca8d76e2000ee29c8cfe12c
sk1fix <agafonovtoha427@gmail.com>
2023-12-10 14:46:30 +0400

    Window

--- main_window.py
+++ main_window.py
@@ -1,2 +1,12 @@
-import csv
-import datetime
+import sys
+from PyQt5 import QtWidgets
+class Window(QtWidgets.QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.init_ui()
+    def init_ui(self):
+        
+        select_folder_button = QtWidgets.QLineEdit()
+        
+        create_annotation_button = QtWidgets.QPushButton('Получить данные в формате YYYY-MM-DD', self)
+        create_annotation_button.clicked.connect(self.check_date)
