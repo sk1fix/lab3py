4be1268b97cd2f4a7e587b6a3119091dcd459434
sk1fix <agafonovtoha427@gmail.com>
2024-02-09 14:10:57 +0400

    new

--- main_window.py
+++ main_window.py
@@ -14,6 +14,8 @@ class Window(QtWidgets.QMainWindow):
         self.select_folder_button = QtWidgets.QPushButton("Выбрать папку")
         self.select_folder_button.clicked.connect(self.select_folder)
 
+        # self.create_dataset_button = QtWidgets.QPushButton("Создать датасет")
+        # self.create_dataset_button.clicked.connect(self.create_dataset)
 
         self.date_input = QtWidgets.QDateEdit()
         self.date_input.setCalendarPopup(True)
@@ -26,6 +28,7 @@ class Window(QtWidgets.QMainWindow):
         layout = QtWidgets.QVBoxLayout()
         layout.addWidget(self.label)
         layout.addWidget(self.select_folder_button)
+        # layout.addWidget(self.create_dataset_button)
         layout.addWidget(self.date_input)
         layout.addWidget(self.get_data_button)
 
@@ -49,7 +52,15 @@ class Window(QtWidgets.QMainWindow):
         main_date = self.date_input.date().toString(QtCore.Qt.ISODate)
         self.find_course(main_date)
 
-    
+    def create_dataset(self):
+        if not self.folderpath:
+            QtWidgets.QMessageBox.warning(self, 'Ошибка', 'Выберите папку с исходным датасетом')
+            return
+
+        self.destination_folder = QtWidgets.QFileDialog.getExistingDirectory(self, 'Выберите папку для нового датасета')
+        if self.destination_folder:
+            self.menu = NewData(self)
+            self.menu.show()
     def find_course(self, main_date):
         course = get_usd(main_date)
         if course == None:
@@ -59,7 +70,30 @@ class Window(QtWidgets.QMainWindow):
             QtWidgets.QMessageBox.information(self, 'Данные получены',
                                               f'Курс доллара на {main_date} равен {course} руб')
 
+class NewData(QtWidgets.QDialog):
+     def __init__(self, Main: Window = None):
+        super(NewData, self).__init__(Main)
+        self.setModal(True)
 
+        if Main is not None:
+            self.Main = Main
+
+        layout = QtWidgets.QVBoxLayout()
+
+        self.button1 = QtWidgets.QPushButton("Разделить на X и Y")
+        self.button1.clicked.connect(self.but1)
+        self.button2 = QtWidgets.QPushButton("Разделить по годам")
+        self.button2.clicked.connect(self.but2)
+        self.button3 = QtWidgets.QPushButton("Разделить по неделям")
+        self.button3.clicked.connect(self.but3)
+
+        layout.addWidget(self.button1)
+        layout.addWidget(self.button2)
+        layout.addWidget(self.button3)
+
+        self.setLayout(layout)
+
+        
 if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv)
     window = Window()
